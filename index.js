2cf38bd287b283d4daac13297ef6031643d7e2a1
Joshua Saylor <joshuasaylor01@gmail.com>
2022-03-23 10:01:36 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const charcs = "abcdefghijklmnopqrstuvwxyzABCDEGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&*"
+const passLength = 10;
+let password1 = "";
+let password2 = [];
+let password3 = [];
+let password4 = [];
+let genPass = document.getElementById("generate");
+let pass1 = document.getElementById("pwd1");
+let pass2 = document.getElementById("pwd2");
+let pass3 = document.getElementById("pwd3");
+let pass4 = document.getElementById("pwd4");
+
+function getPassWord(){
+   /* let randomPwd = Math.floor(Math.random() * passLength)
+   password1 = charcs[randomPwd];
+   console.log(password1);*/
+   console.log("button clicked")
+};
+
+function runPassWord(){
+
+}
+
