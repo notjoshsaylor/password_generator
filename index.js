a297f16940b3a7e121cab8625117bfb1a026b93a
Joshua Saylor <joshuasaylor01@gmail.com>
2022-03-25 13:35:54 -0400

    got random to work

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
 const charcs = "abcdefghijklmnopqrstuvwxyzABCDEGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&*"
 const passLength = 10;
+let randomPwd = "";
 let password1 = "";
-let password2 = [];
-let password3 = [];
-let password4 = [];
+let password2 = "";
+let password3 = "";
+let password4 = "";
 let genPass = document.getElementById("generate");
 let pass1 = document.getElementById("pwd1");
 let pass2 = document.getElementById("pwd2");
 let pass3 = document.getElementById("pwd3");
 let pass4 = document.getElementById("pwd4");
 
-function getPassWord(){
-   /* let randomPwd = Math.floor(Math.random() * passLength)
-   password1 = charcs[randomPwd];
-   console.log(password1);*/
-   console.log("button clicked")
+function runPassWord(){
+   
+   for(let i = 0; i < passLength; i++){
+      random = Math.floor(Math.random() * charcs.length); 
+      password1 += charcs.substring(random, random + 1);    
+   }
+   for(let i = 0; i < passLength; i++){
+      random = Math.floor(Math.random() * charcs.length); 
+      password2 += charcs.substring(random, random + 1);    
+   }
+   for(let i = 0; i < passLength; i++){
+      random = Math.floor(Math.random() * charcs.length); 
+      password3 += charcs.substring(random, random + 1);    
+   }
+   for(let i = 0; i < passLength; i++){
+      random = Math.floor(Math.random() * charcs.length); 
+      password4 += charcs.substring(random, random + 1);    
+   }
+   
+   addPwd()
+   
 };
 
-function runPassWord(){
+function addPwd(){
+  
 
-}
+   pass1.textContent += password1;
+   pass2.textContent += password2;
+   pass3.textContent += password3;
+   pass4.textContent += password4;
+
+};
+
+
 
