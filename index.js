b045be3fc7b0f4cee25d32d9f25ec020c3cb774e
Joshua Saylor <joshuasaylor01@gmail.com>
2022-10-10 17:13:46 -0400

    got password field to reset when button clicked

--- index.js
+++ index.js
@@ -1,46 +1,50 @@
 const charcs = "abcdefghijklmnopqrstuvwxyzABCDEGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&*"
-const passLength = 10;
-let randomPwd = "";
-let password1 = "";
-let password2 = "";
-let password3 = "";
-let password4 = "";
-let genPass = document.getElementById("generate");
-let pass1 = document.getElementById("pwd1");
-let pass2 = document.getElementById("pwd2");
-let pass3 = document.getElementById("pwd3");
-let pass4 = document.getElementById("pwd4");
+const passLength = 10
+let randomPwd = ""
+let password1 = ""
+let password2 = ""
+let password3 = ""
+let password4 = ""
+let genPass = document.getElementById("generate")
+let pass1 = document.getElementById("pwd1")
+let pass2 = document.getElementById("pwd2")
+let pass3 = document.getElementById("pwd3")
+let pass4 = document.getElementById("pwd4")
 
-function runPassWord(){
+function getPassWord(){
+   
+   password1 = ""
+   password2 = ""
+   password3 = ""
+   password4 = ""
    
    for(let i = 0; i < passLength; i++){
-      random = Math.floor(Math.random() * charcs.length); 
-      password1 += charcs.substring(random, random + 1);    
+      random = Math.floor(Math.random() * charcs.length) 
+      password1 += charcs.substring(random, random + 1)
    }
    for(let i = 0; i < passLength; i++){
-      random = Math.floor(Math.random() * charcs.length); 
-      password2 += charcs.substring(random, random + 1);    
+      random = Math.floor(Math.random() * charcs.length) 
+      password2 += charcs.substring(random, random + 1)    
    }
    for(let i = 0; i < passLength; i++){
-      random = Math.floor(Math.random() * charcs.length); 
-      password3 += charcs.substring(random, random + 1);    
+      random = Math.floor(Math.random() * charcs.length) 
+      password3 += charcs.substring(random, random + 1)    
    }
    for(let i = 0; i < passLength; i++){
-      random = Math.floor(Math.random() * charcs.length); 
-      password4 += charcs.substring(random, random + 1);    
+      random = Math.floor(Math.random() * charcs.length) 
+      password4 += charcs.substring(random, random + 1)    
    }
    
    addPwd()
    
-};
+}
 
 function addPwd(){
-  
 
-   pass1.textContent += password1;
-   pass2.textContent += password2;
-   pass3.textContent += password3;
-   pass4.textContent += password4;
+   pass1.textContent = password1
+   pass2.textContent = password2
+   pass3.textContent = password3
+   pass4.textContent = password4
 
 };
 
